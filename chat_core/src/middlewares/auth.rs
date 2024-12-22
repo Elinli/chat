1219ffbd96c3cdf13607629adf5e192c69fcdd72
Eli <elixy@qq.com>
2024-12-22 10:40:46 +0800

    feat: auth token by query and header

--- chat_core/src/middlewares/auth.rs
+++ chat_core/src/middlewares/auth.rs
@@ -1,5 +1,5 @@
 use axum::{
-    extract::{FromRequestParts, Request, State},
+    extract::{FromRequestParts, Query, Request, State},
     http::StatusCode,
     middleware::Next,
     response::{IntoResponse, Response},
@@ -8,38 +8,54 @@ use axum_extra::{
     headers::{authorization::Bearer, Authorization},
     TypedHeader,
 };
+use serde::Deserialize;
 use tracing::warn;
 
 use super::TokenVerify;
+
+#[derive(Debug, Deserialize)]
+pub struct Params {
+    access_token: String,
+}
 
 pub async fn verify_token<T>(State(state): State<T>, req: Request, next: Next) -> Response
 where
     T: TokenVerify + Clone + Send + Sync + 'static,
 {
     let (mut parts, body) = req.into_parts();
-    let req =
+    let token =
         match TypedHeader::<Authorization<Bearer>>::from_request_parts(&mut parts, &state).await {
-            Ok(TypedHeader(Authorization(bearer))) => {
-                let token = bearer.token();
-                match state.verify(token) {
-                    Ok(user) => {
-                        let mut req = Request::from_parts(parts, body);
-                        req.extensions_mut().insert(user);
-                        req
+            Ok(TypedHeader(Authorization(bearer))) => bearer.token().to_string(),
+            Err(e) => {
+                if e.is_missing() {
+                    match Query::<Params>::from_request_parts(&mut parts, &state).await {
+                        Ok(params) => params.access_token.clone(),
+                        Err(e) => {
+                            let msg = format!("parse query params failed: {}", e);
+                            warn!(msg);
+                            return (StatusCode::UNAUTHORIZED, msg).into_response();
+                        }
                     }
-                    Err(e) => {
-                        let msg = format!("verify token failed: {:?}", e);
-                        warn!(msg);
-                        return (StatusCode::FORBIDDEN, msg).into_response();
-                    }
+                } else {
+                    let msg = format!("parse Authorization header failed: {}", e);
+                    warn!(msg);
+                    return (StatusCode::UNAUTHORIZED, msg).into_response();
                 }
             }
-            Err(e) => {
-                let msg = format!("parse Authorization header failed: {}", e);
-                warn!(msg);
-                return (StatusCode::UNAUTHORIZED, msg).into_response();
-            }
         };
+
+    let req = match state.verify(&token) {
+        Ok(user) => {
+            let mut req = Request::from_parts(parts, body);
+            req.extensions_mut().insert(user);
+            req
+        }
+        Err(e) => {
+            let msg = format!("verify token failed: {:?}", e);
+            warn!(msg);
+            return (StatusCode::FORBIDDEN, msg).into_response();
+        }
+    };
 
     next.run(req).await
 }
@@ -54,7 +70,6 @@ mod tests {
     use anyhow::Result;
     use axum::{body::Body, middleware::from_fn_with_state, routing::get, Router};
     use tower::ServiceExt;
-
 
     #[derive(Clone)]
     struct AppState(Arc<AppStateInner>);
@@ -84,7 +99,7 @@ mod tests {
         let dk = DecodingKey::load(decoding_pem)?;
         let state = AppState(Arc::new(AppStateInner { ek, dk }));
 
-        let user = User::new(1, "elixy@qq.com", "Eli Shi");
+        let user = User::new(1, "elixy111@qq.com", "Eli Shi");
 
         let token = state.0.ek.sign(user)?;
 
@@ -100,6 +115,13 @@ mod tests {
 
         let res = app.clone().oneshot(req).await?;
 
+        assert_eq!(res.status(), StatusCode::OK);
+
+        // good token in query params
+        let req = Request::builder()
+            .uri(format!("/?access_token={}", token))
+            .body(Body::empty())?;
+        let res = app.clone().oneshot(req).await?;
         assert_eq!(res.status(), StatusCode::OK);
 
         // with no token
